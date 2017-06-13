3ff2a3e36f7cd4371917ae081451ba5dd99d4d86
laoshi <laoshi@qq.com>
2017-06-13 17:04:46 +0800

    bbbb

---
+++ mmm/mmm/DlgBbbb.cpp
@@ -0,0 +1,34 @@
+// DlgBbbb.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "mmm.h"
+#include "DlgBbbb.h"
+#include "afxdialogex.h"
+
+
+// DlgBbbb 对话框
+
+IMPLEMENT_DYNAMIC(DlgBbbb, CDialogEx)
+
+DlgBbbb::DlgBbbb(CWnd* pParent /*=NULL*/)
+	: CDialogEx(DlgBbbb::IDD, pParent)
+{
+
+}
+
+DlgBbbb::~DlgBbbb()
+{
+}
+
+void DlgBbbb::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(DlgBbbb, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// DlgBbbb 消息处理程序
---
+++ mmm/mmm/DlgBbbb.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+// DlgBbbb 对话框
+
+class DlgBbbb : public CDialogEx
+{
+	DECLARE_DYNAMIC(DlgBbbb)
+
+public:
+	DlgBbbb(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~DlgBbbb();
+
+// 对话框数据
+	enum { IDD = IDD_DIALOG1 };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
